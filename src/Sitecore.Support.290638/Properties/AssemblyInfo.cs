94abd44187554f5d3ea980978b10f937c6d21af1
Paul Stupka <paul.stupka@sitecore.com>
2019-09-09 19:00:11 +0300

    base implementation

---
+++ src/Sitecore.Support.290638/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.290638")]
+[assembly: AssemblyProduct("Sitecore.Support.290638")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("When using the SwitchOnRebuild feature with Azure Search provider, the switch may not be performed on remote instances if the web database is not shared across the instances")]
+
